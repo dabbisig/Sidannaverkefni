39c5c220a36e08cac2a32cace93083009bd46c20
Sigurdur <sigurdurkl@ru.is>
2012-11-29 16:06:33 +0000

    Few modifications

---
+++ TicTacToe/Output/Views/TicTacToeView.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToeView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToeView));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlPlayers = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPlayer2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlayer1 = new System.Windows.Forms.TextBox();
+            this.pnlScoreBoard = new System.Windows.Forms.Panel();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblDraws = new System.Windows.Forms.Label();
+            this.pnlPlayers.SuspendLayout();
+            this.pnlScoreBoard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sláðu inn nafn á leikmanni nr.1";
+            // 
+            // pnlPlayers
+            // 
+            this.pnlPlayers.BackColor = System.Drawing.Color.Transparent;
+            this.pnlPlayers.Controls.Add(this.button1);
+            this.pnlPlayers.Controls.Add(this.txtPlayer2);
+            this.pnlPlayers.Controls.Add(this.label2);
+            this.pnlPlayers.Controls.Add(this.txtPlayer1);
+            this.pnlPlayers.Controls.Add(this.label1);
+            this.pnlPlayers.Location = new System.Drawing.Point(358, 43);
+            this.pnlPlayers.Name = "pnlPlayers";
+            this.pnlPlayers.Size = new System.Drawing.Size(294, 176);
+            this.pnlPlayers.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(158, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.PlayerConfirm_Click);
+            // 
+            // txtPlayer2
+            // 
+            this.txtPlayer2.BackColor = System.Drawing.SystemColors.Window;
+            this.txtPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayer2.Location = new System.Drawing.Point(6, 106);
+            this.txtPlayer2.MaxLength = 15;
+            this.txtPlayer2.Name = "txtPlayer2";
+            this.txtPlayer2.Size = new System.Drawing.Size(259, 29);
+            this.txtPlayer2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sláðu inn nafn á leikmanni nr.2";
+            // 
+            // txtPlayer1
+            // 
+            this.txtPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayer1.Location = new System.Drawing.Point(6, 34);
+            this.txtPlayer1.MaxLength = 15;
+            this.txtPlayer1.Name = "txtPlayer1";
+            this.txtPlayer1.Size = new System.Drawing.Size(259, 29);
+            this.txtPlayer1.TabIndex = 2;
+            // 
+            // pnlScoreBoard
+            // 
+            this.pnlScoreBoard.BackColor = System.Drawing.Color.Transparent;
+            this.pnlScoreBoard.Controls.Add(this.lblDraws);
+            this.pnlScoreBoard.Controls.Add(this.lblPlayer2);
+            this.pnlScoreBoard.Controls.Add(this.lblPlayer1);
+            this.pnlScoreBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlScoreBoard.Location = new System.Drawing.Point(410, 53);
+            this.pnlScoreBoard.Name = "pnlScoreBoard";
+            this.pnlScoreBoard.Size = new System.Drawing.Size(242, 138);
+            this.pnlScoreBoard.TabIndex = 3;
+            this.pnlScoreBoard.Visible = false;
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.Location = new System.Drawing.Point(0, 50);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(0, 31);
+            this.lblPlayer2.TabIndex = 3;
+            this.lblPlayer2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(0, 31);
+            this.lblPlayer1.TabIndex = 1;
+            this.lblPlayer1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblDraws
+            // 
+            this.lblDraws.AutoSize = true;
+            this.lblDraws.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDraws.Location = new System.Drawing.Point(0, 100);
+            this.lblDraws.Name = "lblDraws";
+            this.lblDraws.Size = new System.Drawing.Size(0, 31);
+            this.lblDraws.TabIndex = 4;
+            this.lblDraws.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TicTacToeView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(675, 438);
+            this.Controls.Add(this.pnlScoreBoard);
+            this.Controls.Add(this.pnlPlayers);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TicTacToeView";
+            this.Text = "Tic Tac Toe by Dreggjarnar";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.tictactoePainter);
+            this.pnlPlayers.ResumeLayout(false);
+            this.pnlPlayers.PerformLayout();
+            this.pnlScoreBoard.ResumeLayout(false);
+            this.pnlScoreBoard.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlPlayers;
+        private System.Windows.Forms.TextBox txtPlayer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlScoreBoard;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.Label lblDraws;
+
+
+
+
+
+    }
+}
+
